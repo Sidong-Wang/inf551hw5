920c9393bb59daeba511fbf97fdb360ef48b2992
Sidong Wang <sidongwang@sidongdembp.home>
2019-12-06 02:02:25 -0800

    q3

---
+++ q3.py
@@ -0,0 +1,49 @@
+from pyspark import SparkContext
+
+
+import csv
+import sys
+import re
+
+def map(res):
+    line = res.split(',')
+
+
+    return (line[0],1)
+
+def count(a,b):
+
+    return a+b
+
+
+
+def main(argv):
+    sc = SparkContext(appName="hw5")
+
+    freq = sc.textFile(argv[1])
+    likes = sc.textFile(argv[2])
+
+    rdd1 = freq.map(map)\
+                .reduceByKey(count)
+    rdd2 = likes.map(map)\
+                .reduceByKey(count)
+
+
+    output1 = rdd1.collect()
+    output2 = rdd2.collect()
+    rdd3= sc.parallelize([a[0] for a in output1]).distinct()
+    rdd4 = sc.parallelize([a[0] for a in output2]).distinct()
+    output = rdd3.substract(rdd4).collect()
+
+
+
+
+    with open(argv[3], 'w') as out:
+        out.write('Drinker\n')
+        for i in output:
+
+            out.write(i+'\n')
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
