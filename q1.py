eb2d0a63d47a0bad055f3ce70c6a0fc0dc536450
Sidong Wang <sidongwang@sidongdembp.home>
2019-12-06 00:26:40 -0800

    q1

---
+++ q1.py
@@ -0,0 +1,41 @@
+from pyspark import SparkContext
+
+
+import csv
+import sys
+import re
+
+def map(res):
+    line = res.split(',')
+    pattern = 'Bud.*'
+    if int(line[2])<=5 and re.match(pattern, string):
+        return (line[0], flout(1))
+    if int(line[2])>5 and re.match(pattern, string):
+        return (line[0], float(-1))
+
+def count(a,b):
+    if a<0 or b<0:
+        return -1
+    return a+b
+
+
+
+def main(argv):
+    sc = SparkContext(appName="hw5")
+
+    rdd = sc.textFile(argv[1])\
+            .map(map)\
+            .reduceByKey(count)
+
+    output = rdd.collect()
+
+
+
+    with open(argv[2], 'w') as out:
+        for i in output:
+
+            out.write('%s, %s' % (v[0], v[1]))
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
